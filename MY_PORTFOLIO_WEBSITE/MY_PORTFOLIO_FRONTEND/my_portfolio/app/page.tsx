e31b533ece885a59eadde036dddc1acd515508eb
Paras Dattera <parasdattera555@gmail.com>
2024-08-06 09:32:36 +0530

    c

---
+++ MY_PORTFOLIO_WEBSITE/MY_PORTFOLIO_FRONTEND/my_portfolio/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div>
+        <h1>hello , portfolio</h1>
+      </div>
+    </main>
+  );
+}
