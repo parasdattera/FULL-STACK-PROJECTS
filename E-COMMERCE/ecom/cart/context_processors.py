9c6f4961c632de4fc08173ef381663c400a84b99
Paras Dattera <parasdattera555@gmail.com>
2024-08-22 20:42:30 +0530

    COMMIT

--- E-COMMERCE/ecom/cart/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .cart import Cart
-
-# create context processor so that our cart can work on all pages
-
-
-def cart(request):
-    # return default data from our cart
-    return {'cart':Cart(request)}
-    # now we need to let know our django settings.py file that this context processor exists
